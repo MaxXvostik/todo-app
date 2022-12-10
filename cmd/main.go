b65d00624c70bc08802199068f20e0a07e7c1a95
maxXvostik <xvost-m@mail.ru>
2022-12-10 21:23:40 +0300

    3.1_do_psql

--- cmd/main.go
+++ cmd/main.go
@@ -7,16 +7,27 @@ import (
 	"github.com/maXvostik/todo-app/pkg/handler"
 	"github.com/maXvostik/todo-app/pkg/repositori"
 	"github.com/maXvostik/todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initilizing configs: %s", err.Error())
+	}
 
 	repos := repositori.NewRepositori()
 	services := service.NewService(*repos)
 	handler := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8080"), handler.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s ", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
